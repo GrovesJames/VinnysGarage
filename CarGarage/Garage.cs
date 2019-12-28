b87856c18f4310c8bdb65e41660046c2873d800c
James <triskept@gmail.com>
2019-12-28 13:20:04 -0500

    Fully functional car garage

--- CarGarage/Garage.cs
+++ CarGarage/Garage.cs
@@ -6,6 +6,85 @@ namespace CarGarage
 {
     class Garage
     {
-        
+        public List<Car> Cars = new List<Car>();
+
+        public void AddToGarage(Car Car)
+        {
+            Console.WriteLine("\nYou've added a new car to the garage!");
+            Console.WriteLine("\nPress enter to continue..");
+            Console.ReadLine();
+            Cars.Add(Car);
+        }
+        public void ShowCars()
+        {
+            Console.Clear();
+            Console.WriteLine("Loading info on cars checked into the garage..");
+            foreach (Car car in Cars)
+            {
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("\nCar Year: " + car.Year);
+                Console.WriteLine("\nCar Make: " + car.Make);
+                Console.WriteLine("\nCar Model: " + car.Model);
+                Console.WriteLine("\nCar MPG: " + car.Mileage + "\n");
+                Console.WriteLine("------------------------------------------");
+            }
+            Console.WriteLine("\nPress enter to continue..");
+            Console.ReadLine();
+        }
+        public void ShowCarsStats()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("| Make     | Model    | Year    | Mileage    | Gas    | Performance |");
+            Console.WriteLine("--------------------------------------------------------------\n");
+            foreach (Car Car in Cars)
+            {
+                Console.WriteLine("--------------------------------------------------------------\n");
+                Console.WriteLine("| " + Car.Make + "     | " + Car.Model + "     | " + Car.Year + "      | " + Car.Mileage + "     | " + Car.Gas + "      | " + Car.Performance + "    |\n");
+                Console.WriteLine("--------------------------------------------------------------");
+            }
+            Console.WriteLine("\nPress enter to continue..");
+            Console.ReadLine();
+        }
+        public void DriveAllCars()
+        {
+            foreach (Car Car in Cars)
+            {
+                Car.Gas--;
+            }
+            Console.WriteLine("\nPress enter to continue..");
+            Console.ReadLine();
+        }
+        public void MaintainCars()
+        {
+            foreach (Car Car in Cars)
+            {
+                Car.Performance++;
+            }
+            Console.WriteLine("\nYou've performed maintenance on all cars in the garage!\n");
+            Console.WriteLine("Press enter to continue..");
+            Console.ReadLine();
+        }
+        public Car ChooseCar()
+        {
+            int CarNum = 1;
+
+            Console.Clear();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("\n Select a Car:\n");
+
+            foreach (Car Car in Cars)
+            {
+                Console.WriteLine(CarNum + ". " + Car.Make + " " + Car.Model + "\n");
+                CarNum++;
+            }
+            Console.WriteLine("---------------------");
+            Console.WriteLine("---------------------");
+
+            int CarChoice = Convert.ToInt32(Console.ReadLine());
+            Car chosenCar = Cars[CarChoice - 1];
+            return chosenCar;
+        }
     }
 }
--- CarGarage/game.cs
+++ CarGarage/game.cs
@@ -9,6 +9,8 @@ namespace CarGarage
         public bool Exit = false;
         public string Make { get; set; }
         public string Model { get; set; }
+        public Car chosenCar = new Car();
+        public Garage Garage = new Garage();
 
         public Game()
         {
@@ -22,11 +24,11 @@ namespace CarGarage
             Console.ReadLine();
             Console.Clear();
 
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine("\nVinny: 'AAAAAaaaayyyy!! Welcome in! Let's check in your car!'\n");
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("----------------------------------------------------------");
 
             Console.WriteLine("\nVin: 'What's the make of your car, buddy?'");
             string make = Console.ReadLine();
@@ -41,10 +43,10 @@ namespace CarGarage
             int mileage = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("\nV: 'I'll get you set up with my mechanic right now..'");
-            Console.ReadLine();
 
             Car car = new Car();
             car.AddCar(make, model, year, mileage);
+            Garage.AddToGarage(car);
         }
 
         public void Enter()
@@ -53,19 +55,77 @@ namespace CarGarage
             Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine("\nMechanic: 'Alright buddy, listen up. I got a few options for ya:'\n\n");
-            Console.WriteLine(" 1. Get car info\n");
-            Console.WriteLine(" 2. Get car stats\n");
-            Console.WriteLine(" 3. Take a car out for a test drive\n");
-            Console.WriteLine(" 4. Perform maintenance on a car\n");
+            Console.WriteLine(" 1. Get car info");
+            Console.WriteLine(" 2. Get car stats");
+            Console.WriteLine(" 3. Take a car out for a test drive");
+            Console.WriteLine(" 4. Perform maintenance on a car");
 
-            Console.WriteLine("\n 4. Check in another car\n");
-            Console.WriteLine(" 5. Check in another car\n");
-            Console.WriteLine(" 6. View the info of all cars in the garage\n");
-            Console.WriteLine(" 7. View the stats of all cars in the garage\n");
-            Console.WriteLine(" 8. Perform maintenance on all cars in the garage\n");
+            Console.WriteLine("\n 5. Check in another car");
+            Console.WriteLine(" 6. View the info of all cars in the garage");
+            Console.WriteLine(" 7. View the stats of all cars in the garage");
+            Console.WriteLine(" 8. Perform maintenance on all cars in the garage\n\n");
+
+            Console.WriteLine(" 0. Exit Vinny's garage\n");
             Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine("----------------------------------------------------------");
             int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+            if (menuChoice == 1)
+            {
+                chosenCar = Garage.ChooseCar();
+                chosenCar.InfoCar();
+            }
+            else if (menuChoice == 2)
+            {
+                chosenCar = Garage.ChooseCar();
+                chosenCar.StatsCar();
+            }
+            else if (menuChoice == 3)
+            {
+                chosenCar = Garage.ChooseCar();
+                chosenCar.TestDrive();
+            }
+            else if (menuChoice == 4)
+            {
+                chosenCar = Garage.ChooseCar();
+                chosenCar.Maintenance();
+            }
+            else if (menuChoice == 5)
+            {
+                Console.Clear();
+                Console.WriteLine("\nM: 'What's the make of your new vehicle?'");
+                string make = Console.ReadLine();
+
+                Console.WriteLine("\nM: 'And the model?'");
+                string model = Console.ReadLine();
+
+                Console.WriteLine("\nM: 'What about the year it was made?'");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nM: 'Lastly, the miles you get per gallon?'");
+                int mileage = Convert.ToInt32(Console.ReadLine());
+
+                Car car = new Car();
+                car.AddCar(make, model, year, mileage);
+                Garage.AddToGarage(car);
+
+            }
+            else if (menuChoice == 6)
+            {
+                Garage.ShowCars();
+            }
+            else if (menuChoice == 7)
+            {
+                Garage.ShowCarsStats();
+            }
+            else if (menuChoice == 8)
+            {
+                Garage.MaintainCars();
+            }
+            else if (menuChoice == 0)
+            {
+                Exit = true;
+            }
         }
     }
 }
